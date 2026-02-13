ca1f8e31ef4ea4f27ed6634a940caa950c1d350c
Bécourt Matéo <mateobecourt@gmail.com>
2026-02-13 14:24:38 +0100

    fonction de recherche par titre / type

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,57 @@
-async function afficherOeuvres() {
+window.onload = chargerOeuvres;
+
+async function chargerOeuvres() {
   const res = await fetch("http://localhost:3000/oeuvres");
   const oeuvres = await res.json();
 
-  const container = document.querySelector('#oeuvres');
-  container.innerHTML = "";
+  const tbody = document.querySelector("#tableauOeuvres tbody");
+  tbody.innerHTML = "";
 
   oeuvres.forEach(o => {
-    const li = document.createElement('li');
-    li.textContent = `${o.titre} (${o.type}) - ${o.statut}`;
-    container.appendChild(li);
+    const tr = document.createElement('tr');
+    const tdTitre = document.createElement('td');
+    tdTitre.textContent = o.titre;
+    tr.appendChild(tdTitre);
+
+    const tdType = document.createElement("td");
+    tdType.textContent = o.type;
+    tr.appendChild(tdType);
+
+    const tdStatut = document.createElement("td");
+    tdStatut.textContent = o.statut;
+    tr.appendChild(tdStatut);
+
+    const tdNote = document.createElement("td");
+    tdNote.textContent = o.note;
+    tr.appendChild(tdNote);
+
+    const tdProg = document.createElement("td");
+    tdProg.textContent = o.progression;
+    tr.appendChild(tdProg);
+
+    tbody.appendChild(tr);
+  });
+};
+
+const barreDeRecherche = document.querySelector(".header__search-input")
+
+function rechercheOeuvre() {
+  const recherche = barreDeRecherche.value.toLowerCase();
+  console.log(recherche);
+  const lignes = document.querySelectorAll("#tableauOeuvres tbody tr");
+  lignes.forEach(ligne => {
+    if (!ligne.cells[0].textContent.toLowerCase().includes(recherche) && !ligne.cells[1].textContent.includes(recherche)){
+      ligne.style.display = "none";
+    } else{
+      ligne.style.display = "";
+    }
   });
 }
+/*
+barreDeRecherche.addEventListener("input", rechercheOeuvre);
 
-document.getElementById("bouttonChargementOeuvres").onclick = afficherOeuvres;
+function ajouterOeuvre(){
+  
+}
+
+document.querySelector(".btn-add").addEventListener("click", ajouterOeuvre)*/
